cb6e5d09c5e7de11fef3ef7468c3f608eea6f68d
baldx <mr.ahmad.m2006@gmail.com>
2023-08-03 20:45:24 +0200

    update

--- src/index.js
+++ src/index.js
@@ -1,8 +1,10 @@
 import projectModal from "./modules/projectModal";
+import logic from "./modules/projectModalLogic"
 
 const addProjectBtn = document.querySelector(".addProject");
 
 addProjectBtn.addEventListener("click", () => {
     document.querySelector(".popUpProject").style.display = "block"
+    
 })
 
