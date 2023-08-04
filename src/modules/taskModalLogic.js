8f50449be5a459ec5672e740fbf1fd61ab3a244d
baldx <mr.ahmad.m2006@gmail.com>
2023-08-04 13:45:03 +0200

    add function logic to library & priority

--- src/modules/taskModalLogic.js
+++ src/modules/taskModalLogic.js
@@ -16,6 +16,29 @@ let taskLibrary = [
     }
 ]
 
-function taskLogic() {
+const taskInput = document.querySelector("#task");
+const descriptionInput = document.querySelector("#description");
+const dueDateInput = document.querySelector("#due");
+const container = document.querySelector(".popUpTask");
+const submitBtn = document.querySelector(".submit");
 
+function taskLogic() {
+    submitBtn.addEventListener("click", element => {
+        element.preventDefault();
+        addTaskToLibrary();
+    })
 }
+
+function addTaskToLibrary() {
+    container.style.display = "none";
+    let newTask = new task(taskInput.value, descriptionInput.value, dueDateInput.value, getPriorityValue());
+    taskLibrary.push(newTask);
+}
+
+
+function getPriorityValue() {
+    if (document.querySelector("input[name='priority']:checked").value == "yes") return true;
+    else return false;
+}
+
+export default taskLogic();
