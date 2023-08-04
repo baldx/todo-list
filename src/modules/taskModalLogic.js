beea71799a6491c62700819355c9a14f1bd026a5
baldx <mr.ahmad.m2006@gmail.com>
2023-08-04 13:29:38 +0200

    create constructor & library array

--- src/modules/taskModalLogic.js
+++ src/modules/taskModalLogic.js
@@ -1,3 +1,21 @@
+class task {
+    constructor(task, description, dueDate, priority) {
+        this.task = task;
+        this.description = description;
+        this.dueDate = dueDate;
+        this.priority = priority;
+    }
+}
+
+let taskLibrary = [
+    {
+        task: "Task",
+        description: "Some interesting stuff",
+        dueDate: "06/04/23",
+        priority: "high"
+    }
+]
+
 function taskLogic() {
-    
+
 }
