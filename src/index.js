c96ca1ea46a8e38a844e368527767166c2947e0b
baldx <mr.ahmad.m2006@gmail.com>
2023-08-04 13:28:57 +0200

    import & add task modal event

--- src/index.js
+++ src/index.js
@@ -1,10 +1,15 @@
+import { add } from "date-fns";
 import projectModal from "./modules/projectModal";
+import taskModal from "./modules/taskModal";
 import logic from "./modules/projectModalLogic"
 
 const addProjectBtn = document.querySelector(".addProject");
+const addTaskBtn = document.querySelector(".addTask");
 
 addProjectBtn.addEventListener("click", () => {
     document.querySelector(".popUpProject").style.display = "block"
-    
 })
 
+addTaskBtn.addEventListener("click", () => {
+    document.querySelector(".popUpTask").style.display = "block";
+})
